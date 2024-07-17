c26137a5fdd5040dc9cf0e59edcfa67f0cba9961
VicenteCosmo <vicentecosmo55@gmail.com>
2024-07-17 18:48:00 +0100

    blog2

--- database/connection.cjs
+++ database/connection.cjs
@@ -1,7 +1,7 @@
 const mysql = require('mysql')
 
 
-const DB = mysql.createConnection({
+/*const DB = mysql.createConnection({
     host: 'bwd9njjtfcuriew5oprj-mysql.services.clever-cloud.com',
     user: 'uledvnij62nsqm7h',
     port:'3306',
@@ -17,15 +17,14 @@ DB.connect((e)=>{
     else{
       console.log('database connected')
     }
-})
-
-/*const DB=mysql.createConnection({
+})*/
+const DB=mysql.createConnection({
   host:'localhost',
   user:'root',
   port: '3306',
   password:'cosmo',
   database:'bwd9njjtfcuriew5oprj'
-})*/
+})
 
 DB.connect((e)=>{
   if(e){
